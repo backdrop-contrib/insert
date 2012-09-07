03c2c6f98a05e1d4892631d1c7ce31717f8c970d
Nathan Haug <nate@lullabot.com>
2012-09-06 18:28:28 -0700

    Issue #1126994: 2 double-spaces causes wrong path when using Transliteration.

--- insert.js
+++ insert.js
@@ -95,7 +95,12 @@ Drupal.behaviors.insert.attach = function(context) {
     content = options['content'];
 
     // Cleanup unused replacements.
-    content = content.replace(/__([a-z0-9_]+)__/g, '');
+    content = content.replace(/"__([a-z0-9_]+)__"/g, '""');
+
+    // Cleanup empty attributes (other than alt).
+    content = content.replace(/([a-z]+)[ ]*=[ ]*""/g, function(match, tagName) {
+      return (tagName === 'alt') ? match : '';
+    });
 
     // Insert the text.
     Drupal.insert.insertIntoActiveEditor(content);
